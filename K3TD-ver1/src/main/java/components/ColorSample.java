5f450c9330884f10b698e4d9f9a073911c29e0e5
Võ Dương Khang <114740029+duokhang1676@users.noreply.github.com>
2024-04-02 07:47:27 +0700

    init commit

---
+++ K3TD-ver1/src/main/java/components/ColorSample.java
@@ -0,0 +1,8 @@
+package components;
+
+import java.awt.Color;
+
+public class ColorSample {
+	public static Color gb = new Color(193,219,208,0);
+	public static Color gbHover = new Color(80,120,100);
+}
