b483361f7bad0dd0db9bbfa910a44d157e02a07d
Doww <kay.ngoquoctinh@gmail.com>
2024-04-08 00:39:26 +0700

    add XuatHang panel, database connection

---
+++ K3TD-ver1/src/main/java/db/DBConfiguation.java
@@ -0,0 +1,7 @@
+package db;
+
+public class DBConfiguation {
+	public static String databaseURL = "jdbc:sqlserver://localhost:1433;databaseName=QUANLYHIEUTHUOCTAYK3TD";
+	public static String user = "sa";
+	public static String password = "sapassword";
+}
